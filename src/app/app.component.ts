6476c96148698911f2d2e4612e2440ea7242d6a4
alopezo <8TPFdVPnehjv4d>
2022-04-28 14:49:12 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
+// const htmlToDocx = require('html-to-docx');
+// const saveAs = require('file-saver');
+declare var anime: any;  
+
+interface sourceData {
+  opSelector: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,257 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'roadmap-generator';
+
+  visionSources!: sourceData[];
+  aboutSnomedSources!: sourceData[];
+  currentStateSources!: sourceData[];
+  prioritiesSources!: sourceData[];
+  priorities = new FormControl();
+  futureStateSources!: sourceData[];
+  roadmap: string = '';
+
+  constructor(readonly snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    this.roadmap = `
+      <h1>SNOMED CT Implementation Roadmap</h1>
+      <div class="Vision"></div>
+      <div class="About-SNOMED"></div>
+      <div class="Current-state"></div>
+      <div class="Priorities"></div>
+      <div class="Future-state"></div>
+      `;
+    this.visionSources = [
+      {
+        opSelector: "Ireland",
+        text: `In 2020 SNOMED International published a new Strategy, their vision is that by 2025, Clinical
+        Terminologies will be used globally, which will result in better health and improved patient
+        outcomes, supported by one language of health.<br>
+        Ee need to establish what a 'quality of care model' using clinical terminology for
+        Irish citizens will look like in 2022. This strategy is aligned with the vision and strategy that
+        SNOMED International has developed. The goal of this strategy is to reinforce and enable,
+        quality care and cost effective delivery, through the adoption of standardised clinical
+        terminology across the country.`
+      },
+      {
+        opSelector: "India",
+        text: `Our vision is 'health and wellbeing for all at all ages'. Continuum of care is a concept strongly 
+        advocated by our policy. Citizencentricity, quality of care, better access, universal health coverage, and 
+        inclusiveness are some of the key principles on which the NHP is founded. All these aspirations can be realized 
+        principally by leveraging the power of the digital technologies.`
+      },
+      {
+        opSelector: "United Kingdom",
+        text: `Our vision is that:<br>
+        <ul><li>clinicians can record, share and access patient information that retains the semantic fidelity (meaning) of clinical encounters</li>
+        <li>commissioners, researchers and public health planners can access clinical information for ‘secondary uses’ with the rich detail of the clinical encounters</li></ul>
+        To achieve this vision, we need to build an 'ecosystem' where information standards work together to support the ‘end-to-end’ flow of patient information across the health and care system. Information standards must not be developed in isolation from each other, but must be designed, developed and implemented collectively.
+        <br>
+        Through the implementation of mutually reinforcing information standards, the NHS clinical information system will become a 'learning health system'. This will lead to improved patient outcomes, lower commissioning costs, improved population health and more effective research to drive innovation.`
+      }
+    ];
+    this.aboutSnomedSources = [
+      {
+        opSelector: "Short",
+        text: `SNOMED International1 is a not-for-profit organization that owns and maintains SNOMED CT, the world’s most
+        comprehensive clinical terminology. SNOMED International plays an essential role in improving the health of
+        humankind by determining standards for a codified language that represents groups of clinical terms. SNOMED CT
+        enables healthcare information to be exchanged globally for the benefit of patients/citizens, care providers and other
+        stakeholders.<br><br>
+        With SNOMED CT, users can record patient data more accurately, exchange patient data both within the health care
+        team and with patients, both locally and across borders, to improve patient outcomes. Further, stakeholders can use
+        SNOMED CT in health data and analytics platforms for clinical analytics, population analytics, management analytics,
+        clinical research, applied research, and other research activities to improve health care. `
+      },
+      {
+        opSelector: "Long",
+        text: `SNOMED International1 is a not-for-profit organization that owns and maintains SNOMED CT, the world’s most
+        comprehensive clinical terminology. SNOMED International plays an essential role in improving the health of
+        humankind by determining standards for a codified language that represents groups of clinical terms. SNOMED CT
+        enables healthcare information to be exchanged globally for the benefit of patients/citizens, care providers and other
+        stakeholders.<br><br>
+        With SNOMED CT, users can record patient data more accurately, exchange patient data both within the health care
+        team and with patients, both locally and across borders, to improve patient outcomes. Further, stakeholders can use
+        SNOMED CT in health data and analytics platforms for clinical analytics, population analytics, management analytics,
+        clinical research, applied research, and other research activities to improve health care. <br>
+        SNOMED International strives to determine the best global standards for health terminologies and to engage with the
+        global healthcare community to improve SNOMED CT to better serve the clinical information needs of a diverse range
+        of health care stakeholders.<br><br>
+        National Members are the key to the continued evolution and use of SNOMED CT. SNOMED International has
+        witnessed the growth in its Member base from 28 Members at the start of 2015 to 41 Members, as of July 2021.
+        Incremental to the Member base are affiliate licensees, which expands the use of SNOMED CT into more than eighty countries
+        globally.`
+      },
+      {
+        opSelector: "Test Long",
+        text: `SNOMED International1 is a not-for-profit organization that owns and maintains SNOMED CT, the world’s most
+        comprehensive clinical terminology. SNOMED International plays an essential role in improving the health of
+        humankind by determining standards for a codified language that represents groups of clinical terms. SNOMED CT
+        enables healthcare information to be exchanged globally for the benefit of patients/citizens, care providers and other
+        stakeholders.<br><br>
+        With SNOMED CT, users can record patient data more accurately, exchange patient data both within the health care
+        team and with patients, both locally and across borders, to improve patient outcomes. Further, stakeholders can use
+        SNOMED CT in health data and analytics platforms for clinical analytics, population analytics, management analytics,
+        clinical research, applied research, and other research activities to improve health care. <br>
+        SNOMED International strives to determine the best global standards for health terminologies and to engage with the
+        global healthcare community to improve SNOMED CT to better serve the clinical information needs of a diverse range
+        of health care stakeholders.<br><br>
+        National Members are the key to the continued evolution and use of SNOMED CT. SNOMED International has
+        witnessed the growth in its Member base from 28 Members at the start of 2015 to 41 Members, as of July 2021.
+        Incremental to the Member base are affiliate licensees, which expands the use of SNOMED CT into more than eighty countries
+        globally.<br><br>
+        SNOMED International1 is a not-for-profit organization that owns and maintains SNOMED CT, the world’s most
+        comprehensive clinical terminology. SNOMED International plays an essential role in improving the health of
+        humankind by determining standards for a codified language that represents groups of clinical terms. SNOMED CT
+        enables healthcare information to be exchanged globally for the benefit of patients/citizens, care providers and other
+        stakeholders.<br><br>
+        With SNOMED CT, users can record patient data more accurately, exchange patient data both within the health care
+        team and with patients, both locally and across borders, to improve patient outcomes. Further, stakeholders can use
+        SNOMED CT in health data and analytics platforms for clinical analytics, population analytics, management analytics,
+        clinical research, applied research, and other research activities to improve health care. <br>
+        SNOMED International strives to determine the best global standards for health terminologies and to engage with the
+        global healthcare community to improve SNOMED CT to better serve the clinical information needs of a diverse range
+        of health care stakeholders.<br><br>
+        National Members are the key to the continued evolution and use of SNOMED CT. SNOMED International has
+        witnessed the growth in its Member base from 28 Members at the start of 2015 to 41 Members, as of July 2021.
+        Incremental to the Member base are affiliate licensees, which expands the use of SNOMED CT into more than eighty countries
+        globally.<br><br>
+        SNOMED International1 is a not-for-profit organization that owns and maintains SNOMED CT, the world’s most
+        comprehensive clinical terminology. SNOMED International plays an essential role in improving the health of
+        humankind by determining standards for a codified language that represents groups of clinical terms. SNOMED CT
+        enables healthcare information to be exchanged globally for the benefit of patients/citizens, care providers and other
+        stakeholders.<br><br>
+        With SNOMED CT, users can record patient data more accurately, exchange patient data both within the health care
+        team and with patients, both locally and across borders, to improve patient outcomes. Further, stakeholders can use
+        SNOMED CT in health data and analytics platforms for clinical analytics, population analytics, management analytics,
+        clinical research, applied research, and other research activities to improve health care. <br>
+        SNOMED International strives to determine the best global standards for health terminologies and to engage with the
+        global healthcare community to improve SNOMED CT to better serve the clinical information needs of a diverse range
+        of health care stakeholders.<br><br>
+        National Members are the key to the continued evolution and use of SNOMED CT. SNOMED International has
+        witnessed the growth in its Member base from 28 Members at the start of 2015 to 41 Members, as of July 2021.
+        Incremental to the Member base are affiliate licensees, which expands the use of SNOMED CT into more than eighty countries
+        globally.`
+      }
+    ];
+    this.currentStateSources = [
+      {
+        opSelector: "Placeholder",
+        text: `Describe the current state of classifications and terminologies in you country, what are the limitations laying the 
+        ground for improvements based on the implementation of SNOMED CT.`
+      }
+    ];
+    this.prioritiesSources = [
+      {
+        opSelector: "Engage clinicians",
+        text: `Capacity building is one of the most important aspects of a SNOMED CT Implementation roadmap.`
+      },
+      {
+        opSelector: "Engage stakeholders",
+        text: `Capacity building is one of the most important aspects of a SNOMED CT Implementation roadmap.`
+      },
+      {
+        opSelector: "Engage software specialists",
+        text: `Capacity building is one of the most important aspects of a SNOMED CT Implementation roadmap.`
+      },
+      {
+        opSelector: "Engage with the research community",
+        text: `Capacity building is one of the most important aspects of a SNOMED CT Implementation roadmap.`
+      },
+      {
+        opSelector: "Develop an education and training strategy",
+        text: `Capacity building is one of the most important aspects of a SNOMED CT Implementation roadmap.`
+      },
+      {
+        opSelector: "Establish the NRC",
+        text: `Capacity building is one of the most important aspects of a SNOMED CT Implementation roadmap.`
+      }
+    ];
+
+    this.futureStateSources = [
+      {
+        opSelector: "Placeholder",
+        text: `Deascribe the target scenario, where SNOMED CT is fully implemented, and the describe teh benefits for data entry, 
+        analytics, reporting, billing, and patient care.`
+      }
+    ];
+  }
+
+  changePriorities(section: string, event: any) {
+    if(event.isUserInput) {
+      setTimeout(() => {
+        if (this.priorities.value && this.priorities.value.length) {
+          let prioritiesText = ''
+          this.priorities.value.forEach((loopPriority: any) => {
+            prioritiesText = prioritiesText + `<h3>${loopPriority.opSelector}</h3>${loopPriority.text}<br><br>`;
+          });
+          this.replaceSection(section, `<h2>${section.replace('-',' ')}</h2>${prioritiesText}<br><br>`);
+        } else {
+          this.replaceSection(section, '');
+        }
+      }, 100)
+    }
+  }
+
+  change(section: string, event: any) {
+    if(event.isUserInput) {
+      if (event.source.value == 'Not applicable') {
+        this.replaceSection(section, '');
+      } else {
+        this.replaceSection(section, `<h2>${section.replace('-',' ')}</h2>${event.source.value}<br><br>`);
+      }
+    }
+  }
+
+  replaceSection(section: string, newValue: string) {
+    const parser = new DOMParser();
+    let parsedRoadmap = parser.parseFromString(this.roadmap, "text/html");
+    let sectionNode = parsedRoadmap.querySelector(`.${section}`);
+    sectionNode?.replaceChildren(newValue)
+    let body = parsedRoadmap.querySelector(`body`)?.innerHTML;
+    body = body?.split('&lt;').join('<');
+    body = body?.split('&gt;').join('>');
+    if (body) {
+      this.roadmap = body;
+    }
+    this.animate(section)
+  }
+
+  htmlDecode(input: string) {
+    var doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
+  }
+
+  animate(section: string) {
+    setTimeout(() => {
+      var elements = document.querySelector(`.${section}`);
+      anime({
+        targets: elements,
+        backgroundColor: ['#f0fc03','#fff'],
+        duration: 8000
+      });
+    }, 100)
+  }
+
+  saveToWord() {
+    this.snackBar.openFromComponent(AppSnackComponent, {
+      duration: 3000,
+    });
+    // const htmlString = `<html><body>${this.roadmap}</body></html>`;
+    // const fileBuffer = await htmlToDocx(htmlString, null, {
+    //   table: { row: { cantSplit: true } },
+    //   footer: true,
+    //   pageNumber: true,
+    // });
+  
+    // saveAs(fileBuffer, 'html-to-docx.docx');
+  }
+
 }
+
+@Component({
+  selector: 'app-root-snack',
+  templateUrl: 'app-root-snack.html',
+  styles: [],
+})
+export class AppSnackComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, AppSnackComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppSnackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
