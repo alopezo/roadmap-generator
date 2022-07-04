56ce1c5af19ebf4b9049671012e00ea6faf54767
alopezo <8TPFdVPnehjv4d>
2022-07-04 08:51:26 -0300

    current state updates

--- src/app/roadmap-content/current-states.ts
+++ src/app/roadmap-content/current-states.ts
@@ -1,18 +1,21 @@
 export const currentStates = [
     {
       opSelector: "Customized text",
-      text: `<mark>[INSTRUCTIONS - Please customize the following sample text, or replace it with your own]<br>
-      The current eHealth landscape in <span class='country'>COUNTRY</span> can be summarized as follows:
-      - [N] healthcare regions, each responsible for [add regional healthcare responsibilities] <br>
-      - [N] hospitals ([N] public /[N] private), [N] general practice clinics, and [add other information to indicate healthcare organisation landscape]<br>
-      - [N] secondary care doctors, [N] general practitioners, and [add other information to indicate healthcare provider landscape] <br>
-      - [P]% of hospitals, [P]% of generical practice clinics and [add other relevant healthcare organisations] currently have computerised clinical records <br>
-      - Vendors used include [Add text to describe the vendor landscape in each sector, or describe how clinical systems are locally developed] <br>
-      - The following eHealth standards are used: [add text to describe standards, e.g. HL7 (for messaging), National Minimum Data Set (for common information model)] <br>
-      - The following code systems are used: [add text to describe the terminologies and classifications currently used, where they are used, and for what purpose] <br>
-      - National infrastructure in place includes: [add text to describe any national infrastructure is place, or planned, such as connectivity, national EHR etc] <br>
-      - National legislation in place includes [add text to describe any relevant national or regional legislation in place that may be relevant] <br>
-      - Current challenges in  the eHealth sector include: [add text to describe any relevant challenges or barriers to achieving vision above] <br>
-      [END INSTRUCTIONS]</mark>`
+      text: `<mark>
+      <p>[INSTRUCTIONS - Please customize the following sample text, or replace it with your own]</p>
+      <p>The current eHealth landscape in <span class='country'>COUNTRY</span> can be summarized as follows:</p>
+      <ul>
+        <li>[N] healthcare regions, each responsible for [add regional healthcare responsibilities]</li>
+        <li>[N] hospitals ([N] public /[N] private), [N] general practice clinics, and [add other information to indicate healthcare organisation landscape]</li>
+        <li>[N] secondary care doctors, [N] general practitioners, and [add other information to indicate healthcare provider landscape]</li>
+        <li>[P]% of hospitals, [P]% of general practice clinics and [add other relevant healthcare organisations] currently have computerised clinical records</li>
+        <li>Vendors used include [Add text to describe the vendor landscape in each sector, or describe how clinical systems are locally developed]</li>
+        <li>The following eHealth standards are used: [add text to describe standards, e.g. HL7 (for messaging), National Minimum Data Set (for common information model)]</li>
+        <li>The following code systems are used: [add text to describe the terminologies and classifications currently used, where they are used, and for what purpose]</li>
+        <li>National infrastructure in place includes: [add text to describe any national infrastructure is place, or planned, such as connectivity, national EHR etc]</li>
+        <li>National legislation in place includes [add text to describe any relevant national or regional legislation in place that may be relevant]</li>
+        <li>Current challenges in  the eHealth sector include: [add text to describe any relevant challenges or barriers to achieving vision above]</li>
+      </ul>
+      <p>[END INSTRUCTIONS]</p></mark>`
     }
 ];
