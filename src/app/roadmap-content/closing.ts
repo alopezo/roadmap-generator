b90e6d9a893cc4150309e235a5de439020a17429
alopezo <8TPFdVPnehjv4d>
2022-07-04 09:13:40 -0300

    closings update

--- src/app/roadmap-content/closing.ts
+++ src/app/roadmap-content/closing.ts
@@ -1,12 +1,26 @@
 export const closings = [
     {
         opSelector: "Short",
-        text: `SNOMED CT, the world's most comprehensive clinical terminology, has many potential benefits for the healthcare sector 
+        text: `<p>SNOMED CT, the world's most comprehensive clinical terminology, has many potential benefits for the healthcare sector 
         in <span class='country'>COUNTRY</span>. It enables clinicians to record patient data more accurately and share this data with 
         other healthcare providers in a meaningful way to improve patient outcomes, it enables systems to integrate more effectively
         with clinical decision support knowledge bases, and it enables clinical data to be integrated from disparate systems
-        for clinical analytics, population health management and clinical research.
-        Through the implementation of SNOMED CT following this roadmap, <span class='country'>COUNTRY</span> can achieve these
-        these benefits and deliver on our eHealth goals.'
+        for clinical analytics, population health management and clinical research.</p>
+        <p>Through the implementation of SNOMED CT following this roadmap, <span class='country'>COUNTRY</span> can achieve 
+        these benefits and deliver on our eHealth goals.</p>`
+    },
+    {
+        opSelector: "Long",
+        text: `<p>SNOMED CT, the world's most comprehensive clinical terminology, has many potential benefits for the healthcare sector 
+        in <span class='country'>COUNTRY</span>. It enables clinicians to record patient data more accurately and share this data with 
+        other healthcare providers in a meaningful way to improve patient outcomes, it enables systems to integrate more effectively
+        with clinical decision support knowledge bases, and it enables clinical data to be integrated from disparate systems
+        for clinical analytics, population health management and clinical research.</p>
+        <p>SNOMED CT will be implemented at different levels of the health system, including its integration with clinical records, 
+        national registries, analytics and reporting. It will be introduced as a requirement in data exchange models and 
+        implementation guides. This should be a coordinated effort that includes the clinicians, software developers, public health 
+        officials and technical interoperability architects, among others.</p>
+        <p>Through the implementation of SNOMED CT following this roadmap, <span class='country'>COUNTRY</span> can achieve 
+        these benefits and deliver on our eHealth goals.</p>`
     }
 ];
