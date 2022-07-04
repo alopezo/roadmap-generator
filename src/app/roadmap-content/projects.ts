d2774bde0b692524720b795aff3a4c787d912491
alopezo <8TPFdVPnehjv4d>
2022-07-04 09:03:45 -0300

    update projects

--- src/app/roadmap-content/projects.ts
+++ src/app/roadmap-content/projects.ts
@@ -1,11 +1,17 @@
 export const projects = [
     {
         opSelector: "Customized text",
-        text: `<mark>[INSTRUCTIONS - Please customize the following sample text, or replace it with your own]<br>
-        To deliver on our eHealth strategy for <span class='country'>COUNTRY</span>, SNOMED CT will be implemented 
-        in the following eHealth projects: <br>
-        - [ADD project name(s), use case(s), location(s) and scope(s) of initial implementation focus, e.g. emergency department system upgrade at hospital X]
-        - [ADD national projects if relevant, e.g. national EHR, national clinical registry, national HIE infrastructure]
-        [END INSTRUCTIONS]</mark>`
+        text: `<mark>
+        <p>[INSTRUCTIONS - Please customize the following sample text, or replace it with your own]</P>
+        <p>To deliver on our eHealth strategy for <span class='country'>COUNTRY</span>, SNOMED CT will be implemented 
+        in the following eHealth projects: </p>
+        <ul>
+            <li>[ADD project name(s), use case(s), location(s) and scope(s) of initial implementation focus, e.g. 
+                emergency department system upgrade at hospital X]</li>
+            <li>[ADD national projects if relevant, e.g. national EHR, national clinical registry, national HIE 
+                infrastructure]</li>
+        </ul>
+        <p>[END INSTRUCTIONS]</p>
+        </mark>`
     }
 ];
