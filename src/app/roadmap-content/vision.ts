d71508870a9c8b2b2628b902a4449e9f83e29aaf
alopezo <8TPFdVPnehjv4d>
2022-07-04 08:43:36 -0300

    Visions update

--- src/app/roadmap-content/vision.ts
+++ src/app/roadmap-content/vision.ts
@@ -3,18 +3,18 @@ export const visions = [
     {
     opSelector: "Better health",
     text: `- The use of a unified clinical terminology in health information systems across <span class='country'>COUNTRY</span>
-    will result in better health and improved patient outcomes for all citizens.
+    will result in better health and improved patient outcomes for all citizens.`
     },
     {
     opSelector: "Continuity of care",
-    text: `- Leveraging the power of standardized clinical terminology in digital techologies will allow us to realize our goals of
+    text: `- Leveraging the power of standardized clinical terminology in digital technologies will allow us to realize our goals of
       strengthening the continuity of care services across <span class='country'>COUNTRY</span> and supporting the key principles 
-      of patient centricity and better access to health care for all.
+      of patient centricity and better access to health care for all.`
     },
     {
     opSelector: "Better information",
     text: `- The use of SNOMED CT in healthcare systems will allow clinicians to record, share and access patient information in a way
     that retains the semantic fidelity (meaning) of clinical encounters. Researchers and public health planners will also be able to access
-    clinical data collected from a wide range of healthcare systems and perform meaningful queries across the rich clinical detail.
+    clinical data collected from a wide range of healthcare systems and perform meaningful queries across the rich clinical detail.`
     }
 ];
