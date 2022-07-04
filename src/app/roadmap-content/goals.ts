17a208ea98b89fd9336edf4064a8110074d8639f
LindaJBird <lbi@ihtsdo.org>
2022-07-04 10:34:47 +1000

    Update goals.ts
    Reviewed by Linda

--- src/app/roadmap-content/goals.ts
+++ src/app/roadmap-content/goals.ts
@@ -1,31 +1,35 @@
-export const goalsIntro = `We have defined the following goals for the strategy:`;
+export const goalsIntro = `The adoption of SNOMED CT will support the following eHealth goals:`;
 export const goals = [
     {
-    opSelector: "Standardising health record data",
-    text: `<p>SNOMED CT will be defined as the standard for capturing data in clinical systems, including the domains of diagnosis, 
-    therapeutic and diagnosis procedures, medications, immunizations, and allergies. 
-    When SNOMED CT is integrated in clinical systems it allows practitioners to accurately represent clinical concepts, generating datasets 
-    ready to be used in analytics, quality metrics, decision support and other use cases.
-    In the cases wehere local code systems or classifications are required for business reasons, these will be mapped with SNOMED CT, so the primary data entry 
-    can be standardized to use SNOMED CT and information can be transformed into this vocabularies as necessary.</p>`
+    opSelector: "Standardization of health record data",
+    text: `<p>SNOMED CT will be defined as the standard for capturing data in clinical systems, including the domains of diagnoses, 
+    therapeutic and diagnostic procedures, medications, immunizations, and allergies. 
+    When SNOMED CT is implemented in clinical systems it allows practitioners to accurately represent clinical concepts, generating datasets 
+    ready to be used in analytics, quality metrics, decision support and many other use cases.
+    In cases where local code systems or classifications are required for reporting purposes, a map will be developed from SNOMED CT to the other 
+    code system to enable clinical data entry to be standardized using SNOMED CT and then transformed as required. </p>`
     },
     {
     opSelector: "Data exchange",
     text: `<p>SNOMED CT will be defined as the common reference terminology for sharing data between facilities and/or healthcare providers. Information 
-    exchange is the key to implement continuity of care and patient centered care. Implementation guides that combine the benefits of SNOMED CT in 
-    combination with HL7 FHIR resources will ensure the transmission of information and meaning between different systems.</p>`
+    exchange is the key to implement continuity of care and patient centered care. Implementation guides that combine the benefits of SNOMED CT with 
+    a common information model (e.g. HL7 FHIR resources) will ensure the consistent transmission of information and meaning between different systems.</p>`
     },
     {
     opSelector: "Clinician support",
     text: `<p>SNOMED CT will be defined as the standard for represeting clinical knowledge in decision support systems. 
     Clinical decision support tools link patient information to clinical knowledge, protocols or guidelines, providing alerts and advice 
-    to clinicians during the care of the patient. SNOMED CT provides the adequate level of detail to encode knowledge rules conditions and actions.
-    Using a SNOMED CT it's possible to share knowledge databases and access a wide market of vendors that provide services using the standard.</p>`
+    to clinicians during the care of the patient. SNOMED CT provides the level of detail needed to encode knowledge rules, conditions and actions.
+    Using SNOMED CT it's possible to share knowledge databases and access a wide market of vendors that provide services using the standard.</p>`
+    },
+    {
+    opSelector: "Population health",
+    text: `<p>Data analysts supporting population health will be able to ...</p>`
     },
     {
     opSelector: "Clinical research",
-    text: `<p>Researchers will be trained to leverage SNOMED CT avantages for data analytics in clinical research. The availability of large SNOMED CT 
+    text: `<p>Researchers will be trained to leverage the power of SNOMED CT for clinical research. The availability of large SNOMED CT 
     encoded datasets opens many opportunities for very accurate patient selection for inclusion in clinical trials, it facilitates retrospective 
-    studies, and opens the door to the realization real world evidence studies.</p>`
+    studies, and opens the door to the realization of real world evidence-based studies.</p>`
     }
 ];
